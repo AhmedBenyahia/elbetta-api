661c25d08642113c7095d34de789ad27c79be8da
Ahmed Benyahia <ahmed.benyahia@dqlick.com>
2020-11-06 12:30:35 +0100

    Create project structure, configure DB and code style

---
+++ src/main/java/com/tekup/gld/project/elbetta/ElbettaApplication.java
@@ -0,0 +1,21 @@
+package com.tekup.gld.project.elbetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Elbetta application.
+ */
+@SpringBootApplication
+public class ElbettaApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ElbettaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tekup/gld/project/elbetta/ElbettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekup.gld.project.elbetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElbettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

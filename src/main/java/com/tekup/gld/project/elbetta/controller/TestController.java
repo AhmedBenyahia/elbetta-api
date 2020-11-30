ceb8ba4b3a202153c55520fd7a7e4d052bb1c594
Hana derwish <derwishhana1@gmail.com>
2020-11-30 13:12:15 +0100

    update local project

--- src/main/java/com/tekup/gld/project/elbetta/controller/TestController.java
+++ src/main/java/com/tekup/gld/project/elbetta/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.tekup.gld.project.elbetta.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/tekup/gld/project/elbetta/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tekup.gld.project.elbetta.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tekup.gld.project.elbetta.models.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity,Long>{
-
-}

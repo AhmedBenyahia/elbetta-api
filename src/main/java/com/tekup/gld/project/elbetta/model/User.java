ad25e5681c39e554e92d2e4c63b1c496963d7416
Ahmed Benyahia <ahmed.benyahia@dqlick.com>
2020-12-08 16:15:19 +0100

    code style check

--- src/main/java/com/tekup/gld/project/elbetta/model/User.java
+++ src/main/java/com/tekup/gld/project/elbetta/model/User.java
@@ -10,7 +10,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
